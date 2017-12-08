121d533f5d7b6b085f3e5ac28e4c58b0d1af05b4
Jackie Coulston <jaxievr@gmail.com>
2017-12-07 19:00:44 -0600

    added styling, alerts for empty inputs

--- dist/bundle.js
+++ dist/bundle.js
@@ -139,15 +139,15 @@ module.exports = { getSpanish };
 "use strict";
 
 function getTagalog() {
-let tagalog = {
-    "Merry":"Maligayang",
-    "Christmas!":"Pasko",
-    "And don’t":"Huwag",
+  let tagalog = {
+    "Merry": "Maligayang",
+    "Christmas!": "Pasko",
+    "And don’t": "Huwag",
     "forget": "kalimutang",
-    "to plan":"magplano",
-    "before":"bago",
-    "you":"ka",
-    "code!":"mag-code!"
+    "to plan": "magplano",
+    "before": "bago",
+    "you": "ka",
+    "code!": "mag-code!"
   };
 
   let frenchChecked = document.getElementById("french");
@@ -157,41 +157,58 @@ let tagalog = {
   let japaneseChecked = document.getElementById("japanese");
   let spanishChecked = document.getElementById("spanish");
   let tagalogChecked = document.getElementById("tagalog");
-  
+
   convertButton.addEventListener("click", toTagalog);
   clearButton.addEventListener("click", clearValue);
-  
-  function printToDom (value) {
-      document.getElementById("output").innerHTML = value;
+
+  function printToDom(value) {
+    document.getElementById("output").innerHTML = value;
   }
-  
+
   function toTagalog(input) {
-      if (tagalogChecked.checked) {
+    if (isInputComplete() && tagalogChecked.checked) {
       let userText = document.getElementById("langInput").value;
       let translation = "";
       let tagalogArray = userText.split(" ");
-      for (let i = 0; i < tagalogArray.length; i++){
-      let sort = tagalogArray[i];
-      if (tagalog[sort]) { //if my object is same as array created by string
+      for (let i = 0; i < tagalogArray.length; i++) {
+        let sort = tagalogArray[i];
+        if (tagalog[sort]) {
+          //if my object is same as array created by string
           translation += tagalog[sort] + " ";
+        }
       }
+      printToDom(translation);
+    }
   }
-       printToDom(translation);
+
+  function isInputComplete(){
+    let userText = document.getElementById("langInput").value;
+    if(userText != "Merry Christmas! And don't forget to plan before you code!"){
+        window.alert("Please type the correct phrase!");
+        return false;
+    }
+    // Add this to IF statement once japanese is done -- !japaneseChecked.checked && 
+    else if(!tagalogChecked.checked && 
+        !frenchChecked.checked && 
+        !spanishChecked.checked){
+        window.alert("Please select a language to translate!");        
+        return false;
+    }
+    return true;
   }
-  }
-  
-  
+
   function clearValue() {
-      let userText = document.getElementById("langInput");
-      userText.value = "";
-      // clearButton = "";
-      textTranslated.innerHTML = "";
-      frenchChecked.checked = false;
+    let userText = document.getElementById("langInput");
+    userText.value = "";
+    // clearButton = "";
+    textTranslated.innerHTML = "";
+    frenchChecked.checked = false;
     //   japaneseChecked.checked = false;
-      spanishChecked.checked = false;
-      tagalogChecked.checked = false;
+    spanishChecked.checked = false;
+    tagalogChecked.checked = false;
   }
 }
 
-module.exports = {getTagalog};
+module.exports = { getTagalog };
+
 },{}]},{},[2]);
